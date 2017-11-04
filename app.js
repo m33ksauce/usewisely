d57a247379b7a3317c1f015d028722fa01a930dc
Chris Meek <m33ksauce@gmail.com>
2017-11-04 14:04:16 -0500

    Adding more routing logic, plus templating

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var express = require('express');
 
 var app = express();
 var port = config.port;
+app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-    res.send('here\'s my app');
+    res.render('index')
+});
+
+app.get('/:year/:month/:day', function (req, res) {
+    res.render('date')
+    //res.send(req.params);
 });
 
 app.listen(port, function() {
