5c0e3331fe74aa12a9116c00e0e18aeff0a22c90
Chris Meek <chris.meek@united.com>
2017-12-05 22:10:16 -0600

    Got the month dropdown working. Still sorting out the max days

--- months.js
+++ months.js
@@ -1,50 +1,62 @@
 var months = function() {
     var months = {
         'january': {
+            formatted: 'January',
             maxDays: 31,
             order: 0
         },
         'february': {
+            formatted: 'February',
             maxDays: 29,
             order: 1
         },
         'march': {
+            formatted: 'March',
             maxDays: 31,
             order: 2
         },
         'april': {
+            formatted: 'April',
             maxDays: 30,
             order: 3
         },
         'may': {
+            formatted: 'May',
             maxDays: 31,
             order: 4
         },
         'june': {
+            formatted: 'June',
             maxDays: 30,
             order: 5
         },
         'july':{
+            formatted: 'July',
             maxDays: 31,
             order: 6
         },
         'august': {
+            formatted: 'August',
             maxDays: 31,
             order: 7
         },
         'september': {
+            formatted: 'September',
             maxDays: 30,
             order: 8
         },
         'october': {
+            formatted: 'October',
             maxDays: 31,
             order: 9
         },
         'november': {
+            formatted: 'November',
             maxDays: 30,
             order: 10
         },
         'december': {
+            formatted: 'December',
             maxDays: 31,
             order: 11
         }
@@ -63,7 +75,7 @@ var months = function() {
     }
 
     var getAll = function() {
-        return Object.getOwnPropertyNames(months);
+        return months;
     }
     
 
